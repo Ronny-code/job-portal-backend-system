ddeb332cc97a02256631e5d642c30a08dd780d75
Ronny-code <rohannaikwade5353@gmail.com>
2026-01-24 00:27:54 +0530

    job module created

---
+++ src/main/java/com/job/portal/controller/JobController.java
@@ -0,0 +1,30 @@
+package com.job.portal.controller;
+
+import com.job.portal.repository.JobRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.job.portal.model.Job;
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @PostMapping
+    public Job createJob(@RequestBody Job job) {
+        return jobRepository.save(job);
+    }
+
+    @GetMapping
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Job getJob(@PathVariable Long id) {
+        return jobRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Job not found"));
+    }
+}
---
+++ src/main/java/com/job/portal/repository/JobRepository.java
@@ -0,0 +1,7 @@
+package com.job.portal.repository;
+import com.job.portal.model.Job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobRepository extends JpaRepository<Job , Long> {
+}
