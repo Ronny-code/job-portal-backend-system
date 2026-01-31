64125e2a5a514e29782f8dfa4f11a7beb6f7d5ca
Ronny-code <rohannaikwade5353@gmail.com>
2026-02-01 01:35:09 +0530

    error solve

---
+++ src/main/java/com/job/portal/security/JwtUtil.java
@@ -0,0 +1,27 @@
+package com.job.portal.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+
+public class JwtUtil {
+
+    private static final String SECRET = "mysecretkeymysecretkeymysecretkey123456";
+    private static final SecretKey key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day
+                .signWith(key)
+                .compact();
+    }
+}
+
+
